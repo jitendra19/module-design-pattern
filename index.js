ccfcbb0a5907946cb0e6ca93bfb99b4490ac3d4d
jitendra19 <jiten.cs2010@gmail.com>
2021-04-23 23:43:44 +0530

    added programs for module pattern

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-// Import stylesheets
-import './style.css';
+// through the use of a closure we expose an object
+// as a public API which manages the private objects array
+var collection = (function() {
+  // private members
+  var objects = [];
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+  // public members
+  return {
+    addObject: function(object) {
+      objects.push(object);
+    },
+    removeObject: function(object) {
+      var index = objects.indexOf(object);
+      if (index >= 0) {
+        objects.splice(index, 1);
+      }
+    },
+    getObjects: function() {
+      return JSON.parse(JSON.stringify(objects));
+    }
+  };
+})();
+
+collection.addObject("Bob");
+collection.addObject("Alice");
+collection.addObject("Franck");
+// prints ["Bob", "Alice", "Franck"]
+console.log(collection.getObjects());
+collection.removeObject("Alice");
+// prints ["Bob", "Franck"]
+console.log(collection.getObjects());
